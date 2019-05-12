1813857843810f0db744e088cb8ad2078bbdc2cf
Karine Rocha <karinerocha.s@hotmail.com>
2019-05-12 17:01:16 -0300

    reducer

---
+++ src/app/reducers/reducers.ts
@@ -0,0 +1,39 @@
+import { ActionReducerMap, createSelector, createFeatureSelector, ActionReducer, MetaReducer } from '@ngrx/store';
+
+import { environment } from '../../environments/environment';
+import { RouterStateUrl } from '../../shared/utils';
+
+import * as fromRouter from '@ngrx/router-store';
+import { storeFreeze } from 'ngrx-store-freeze'
+
+import * as fromAuth from '../auth/reducers/auth.reducer';
+import { FormatInputPathObject } from 'path';
+
+export interface State {
+    auth: fromAuth.state,
+    router: fromRouter.RouterReducerState<RouterStateUrl>;
+}
+
+export const reducers: ActionReducerMap<State | any> = {
+    auth: fromAuth.AuthReducer,
+    router: fromRouter.routerReducer
+}
+
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+    return function (state: State, action: any): State {
+        console.log('state', state);
+        console.log('action', action);
+        return reducer(state, action);
+    }
+}
+
+export const metaReducer: MetaReducer<State>[] = !environment.production
+    ? [logger, storeFreeze]
+    : []
+
+export const getAuthState = createFeatureSelector<fromAuth.state>('auth');
+
+export const getAuth = createSelector(
+    getAuthState,
+    fromAuth.getAuthState
+)
